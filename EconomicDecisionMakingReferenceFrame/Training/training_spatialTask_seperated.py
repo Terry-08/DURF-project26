9483650aa871972b25ad1f05f127d4f59fd93a67
Terry-08 <terrylzc.me@gmail.com>
2026-07-15 13:35:47 +0800

    update several parameters

--- EconomicDecisionMakingReferenceFrame/Training/training_spatialTask_seperated.py
+++ EconomicDecisionMakingReferenceFrame/Training/training_spatialTask_seperated.py
@@ -12,12 +12,12 @@ os.makedirs(saveRoot+taskTrainName, exist_ok=True)
 # task and model parameters
 dt = 10 # The simulation timestep.
 tau = 100 # The intrinsic time constant of neural state decay.
-T = 4000 # The trial length.
+T = 3500 # The trial length.
 N_trials_per_condition = 4 # The number of trials per training update.
 dd = DelayedEconomicDecision_SeparatedSpatialTask(dt=dt, tau=tau, T=T, N_trials_per_condition=N_trials_per_condition,target_delay_duration=500,wait_duration=500)
 
 offer_pair_test = [(iA*0.5,iB*0.5*1.7) for iA in range(9) for iB in range(9)]
-dd_test = DelayedEconomicDecision_SeparatedSpatialTask(dt=dt, tau=tau, T=4000, target_delay_duration=500,wait_duration=500,
+dd_test = DelayedEconomicDecision_SeparatedSpatialTask(dt=dt, tau=tau, T=T, target_delay_duration=500,wait_duration=500,
                                                        N_trials_per_condition=10,offer_pairs=offer_pair_test)
 
 
@@ -56,7 +56,7 @@ train_params['performance_measure'] = performance_measure
 train_params['performance_cutoff'] = .99
 
 ## -------- Training loop ##
-ensembleSize=50
+ensembleSize=40
 for netii in range(ensembleSize):
     startTime = datetime.datetime.now().strftime('%Y%m%d-%H-%m')
 
